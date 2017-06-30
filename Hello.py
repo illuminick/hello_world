3042df2a2eef2bf5670cab679686abb8db09f058
illuminick <karlkontus@gmail.com>
2017-06-30 08:29:49 +0100

    Added my first Python file

---
+++ Hello.py
@@ -0,0 +1,9 @@
+# Here's a comment
+print('Hello world!')
+print('What is your name?')
+myName = input()
+print('Nice to meet you, ' + myName)
+print('The length of your name is:' + str(len(myName)))
+print('What is your age?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
